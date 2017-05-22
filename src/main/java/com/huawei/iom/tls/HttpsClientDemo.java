2f0694993f58046c3c85273d4c45d236262cdd30
zhangxin218 <zhangxin_bsq@sina.com>
2017-05-22 10:35:08 +0800

    Update HttpsClientDemo.java
    commit 4

--- src/main/java/com/huawei/iom/tls/HttpsClientDemo.java
+++ src/main/java/com/huawei/iom/tls/HttpsClientDemo.java
@@ -155,6 +155,7 @@ public class HttpsClientDemo extends DefaultHttpClient {
         System.out.println("1");
         System.out.println("2");
         System.out.println("3");
+        System.out.println("4");
     }
 
 }
